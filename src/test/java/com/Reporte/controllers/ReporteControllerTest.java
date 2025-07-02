5c6c3f170bc2d7c1c117fe251de2dd1db50c7f7a
natanael reveco <reveconatanael03@gmail.com>
2025-07-02 00:13:52 -0400

    se agrego una prueba con junit

---
+++ src/test/java/com/Reporte/controllers/ReporteControllerTest.java
@@ -0,0 +1,15 @@
+package com.Reporte.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class ReporteControllerTest {
+    @Test
+    void testInventario() {
+
+    }
+
+    @Test
+    void testVentas() {
+
+    }
+}
